e00c493c19bafc554dedb87422f0fad3717209d1
Simran2624 <103874153+Simran2624@users.noreply.github.com>
2023-11-06 07:29:09 +0530

    Update Exp_4.py

--- Exp_4.py
+++ Exp_4.py
@@ -1,5 +1,6 @@
 import csv
 
+#Method to read each row of file and return as string
 def read_csv_file(file_path):
     with open(file_path, 'r') as file:
         csv_reader = csv.reader(file)
@@ -7,6 +8,7 @@ def read_csv_file(file_path):
     strings = [','.join(row) for row in data]
     return strings
 
+#Define file path and call the method
 file_path = "C:/Users/admin/OneDrive/Desktop/user/UG/Data Wrangling Lab/samplecsv.csv"
 strings = read_csv_file(file_path)
 print(strings)
